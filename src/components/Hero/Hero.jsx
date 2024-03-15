f6532f48abccf52e706bd792c1e97805136af056
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2024-03-15 16:02:10 +0200

    add components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,10 @@
-import * as s from "./Hero.styled";
+import * as s from './Hero.styled';
 
 const Hero = () => {
   return (
     <s.Container>
       <s.TitleContainer>
         <s.Title>Empowering Health</s.Title>
-        <s.Title>Enriching Lives</s.Title>
       </s.TitleContainer>
     </s.Container>
   );
